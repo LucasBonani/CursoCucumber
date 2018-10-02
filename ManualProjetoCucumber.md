2dae52aadd9eb49a9b8b4759b9bb723648ca4be6
Lucas <lucas.bonani@gmail.com>
2018-10-01 21:08:24 -0300

    atualização

---
+++ ManualProjetoCucumber.md
@@ -0,0 +1,8 @@
+CursoCucumber
+
+sites de apoio para modificar grupos de variáveis:
+
+https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24
+
+https://regex101.com/
+
--- src/test/java/AprenderCucumberSteps.java
+++ src/test/java/AprenderCucumberSteps.java
@@ -1,7 +1,11 @@
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
-import cucumber.api.SnippetType;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
@@ -48,4 +52,63 @@ public class AprenderCucumberSteps {
 		// e para JUnit será como passo error e cenário error
 		//throw new RuntimeException();
 	}
+	
+	Date entrega = new Date();
+	
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if(tempo.equals("dias")) {
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if(tempo.equals("meses")) {
+			cal.add(Calendar.MONTH, arg1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dateFormatada = format.format(entrega);
+		Assert.assertEquals(data, dateFormatada);
+	}
+	
+	/***  DESAFIO  ***/ 
+	
+	@Dado("^que o ticket( especial)? é A.(\\d+)$")
+	public void queOTicketÉ(String tipo , int arg1) throws Throwable {
+	}
+
+	@Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+	public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
+	}
+
+	@Dado("^que o nome do passageiro é \"(.*)\"$")
+	public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
+	}
+
+	@Dado("^que o telefone do passageiro é (\\d+)-(\\d+)$")
+	public void queOTelefoneDoPassageiroÉ(int arg1, int arg2) throws Throwable {
+	}
+
+	@Quando("^criar os steps$")
+	public void criarOsSteps() throws Throwable {
+	}
+
+	@Então("^o teste vai funcionar$")
+	public void oTesteVaiFuncionar() throws Throwable {
+	}
+	
 }
+
